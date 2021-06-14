217dab433c019ed00d5ed770f4e52bad90aac34a
Gaung Alif <gaungalif@gmail.com>
2021-06-15 01:19:48 +0800

    initialize standard directory and files

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ curr_dir = os.getcwd()
 sys.path.append(curr_dir)
 
 from cifar.models.models import MyNetwork
-from train import *
+import train
 import torch
 
 from pathlib import Path
@@ -14,17 +14,15 @@ from pathlib import Path
 
 
 net = MyNetwork(ichan=3, clazz=10, imsize=(64,64))
-state_dict = net.state_dict()
-# fpath = '/kaggle/working/mynetwork_state_dict.pth'
-loaded_state_dict = torch.load(Path(curr_dir).joinpath('mynetwork_state_dict.pth'), map_location='cpu')
+loaded_state_dict = torch.load(train.fpath, map_location='cpu')
 net.load_state_dict(loaded_state_dict)
 
 idx = 0
 
-imgs, lbls = next(iter(valid_loader))
+imgs, lbls = next(iter(train.valid_loader))
 out = net(imgs)
 out = torch.softmax(out, dim=0)
 out = torch.argmax(out, dim=1)
 # torch.argmax(out[0])
-for idx in len(out):
-    print('data asli= ', out[idx],' data hasil =', lbls[idx])
+for idx in range(len(out)):
+    print(out[idx]==lbls[idx])
--- train.py
+++ train.py
@@ -14,20 +14,19 @@ from cifar.datasets import loader
 from cifar.models.models import MyNetwork
 from cifar.trainer import task
 
+base_dir = Path(curr_dir).joinpath('dataset')
+train_loader, trainset = loader.train_loader(base_dir)
+valid_loader, validset = loader.valid_loader(base_dir)
+
+print(len(validset)), print(len(trainset))
+
+clazz = 10
+net = MyNetwork(ichan=3, clazz=clazz, imsize=(64,64)).to(task.device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 if __name__ == '__main__':
-    base_dir = Path(curr_dir).joinpath('dataset')
-    train_loader, trainset = loader.train_loader(base_dir)
-    valid_loader, validset = loader.valid_loader(base_dir)
-
-    print(len(validset)), print(len(trainset))
-
-    clazz = 10
-    net = MyNetwork(ichan=3, clazz=clazz, imsize=(64,64)).to(task.device)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
     train = task.train_network(2, train_loader, valid_loader, net, criterion, optimizer, log_freq=20)
-    state_dict = net.state_dict()
-    fpath = Path(curr_dir).joinpath('mynetwork_state_dict.pth')
-    torch.save(state_dict, fpath)
-    
+
+state_dict = net.state_dict()
+fpath = Path(curr_dir).joinpath('mynetwork_state_dict.pth')
+torch.save(state_dict, fpath)
