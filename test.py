a3f7168dc42a6a05bbc4c55629c79b7b40f78726
Gaung Alif <gaungalif@gmail.com>
2021-06-15 00:37:39 +0800

    initialize standard directory and files

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ import torch
 net = MyNetwork(ichan=3, clazz=10, imsize=(64,64))
 state_dict = net.state_dict()
 fpath = '/kaggle/working/mynetwork_state_dict.pth'
-loaded_state_dict = torch.load(fpath, map_location='gpu')
+loaded_state_dict = torch.load(fpath, map_location='cpu')
 net.load_state_dict(loaded_state_dict)
