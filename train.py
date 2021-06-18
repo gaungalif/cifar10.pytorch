1b3e8f78b76424cb5f099eb61336485b6c9d1af6
Gaung Alif <gaungalif@gmail.com>
2021-06-18 22:40:55 +0800

    feat: add SGD and Adam optimizer

--- train.py
+++ train.py
@@ -24,12 +24,15 @@ if __name__ == "__main__":
     parser.add_argument('-lr','--lrate', type=float, help='learning rate', required=True)
     parser.add_argument('-ep','--epoch',type=int, help='epoch', required=True)
     parser.add_argument('-lf','--lfreq',type=int, help='log frequency')
-    parser.add_argument('-m','--momentum',type=float, help='momentum', required=True)
+    parser.add_argument('-m','--momentum',type=float, help='momentum', default=0.9)
     parser.add_argument('-b','--bsize',type=int, help='batch size', default=32)
     parser.add_argument('-n','--num_worker',type=int, help='num worker', default=8)
     parser.add_argument('-sp','--save_path',type=str, help='save path', required=True)
+    parser.add_argument('-o','--optimizer',type=str, help='optimizer', required=True)
     args = parser.parse_args()
     
+    
+
     LR = args.lrate
     EPOCHS = args.epoch
     LOG_FREQ = args.lfreq
@@ -38,6 +41,16 @@ if __name__ == "__main__":
     BSIZE = args.bsize
     NUM_WORKER = args.num_worker
     save_path = args.save_path
+    
+    net = MyNetwork(ichan=3, clazz=10, imsize=(64,64)).to(task.device)
+
+    OPTIMIZER = args.optimizer
+    if OPTIMIZER == 'sgd':
+        OPTIMIZER = optim.SGD(net.parameters(), LR, MOMENTUM)
+    elif OPTIMIZER == 'adam':
+        OPTIMIZER = optim.Adam(net.parameters(), LR)
+    else:
+        print("error cuy")
 
     curr_dir = Path(curr_dir)
     base_dir = curr_dir.joinpath('dataset')
@@ -45,10 +58,9 @@ if __name__ == "__main__":
     valid_loader, validset = loader.valid_loader(base_dir, BSIZE, NUM_WORKER)
     fpath = curr_dir.joinpath(save_path)
 
-    net = MyNetwork(ichan=3, clazz=10, imsize=(64,64)).to(task.device)
+    
     criterion = nn.CrossEntropyLoss()
     state_dict = net.state_dict()
-
-    optimizer = optim.SGD(net.parameters(), LR, MOMENTUM)
-    task.train_network(EPOCHS, train_loader, valid_loader, net, criterion, optimizer, LOG_FREQ)  
+    print(OPTIMIZER)
+    task.train_network(EPOCHS, train_loader, valid_loader, net, criterion, OPTIMIZER, LOG_FREQ)  
     torch.save(state_dict, fpath)
