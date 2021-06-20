678312dd93aeedea90a121a938eb042d7a5536e8
Gaung Alif <gaungalif@gmail.com>
2021-06-21 00:45:18 +0800

    feat: new progress meter and average meter

--- cifar/trainer/task.py
+++ cifar/trainer/task.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+import time
+import warnings
 
 import torch
 from tqdm.notebook import tqdm 
@@ -6,52 +9,149 @@ from tqdm.notebook import tqdm
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
 
 def train_batch(epoch, dataloader, net, criterion, optimizer, log_freq=2000):
-    running_loss = 0.0
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(dataloader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch))
+
+    net.train()
+
+    end = time.time()  
+
     for i, data in tqdm(enumerate(dataloader, 0)):
+        data_time.update(time.time() - end)
+
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
+        output = net(inputs)
+        loss = criterion(output,labels)
+
+        acc1, acc5 = accuracy(output,labels, topk=(1,5))
+        losses.update(loss.item(), inputs.size(0))
+        top1.update(acc1[0], inputs.size(0))
+        top5.update(acc5[0], inputs.size(0))
+
         
         optimizer.zero_grad()
-        out = net(inputs)
-        loss = criterion(out,labels)
         loss.backward()
         optimizer.step()
 
-        # print statistics
-        # print(loss)
-        running_loss += loss.cpu().item()
-        if i % log_freq == log_freq-1:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / log_freq))
-            running_loss = 0.0
+        batch_time.update(time.time() - end)
+        end = time.time()
+        
+        if i % log_freq == 0:
+            progress.display(i)
+
+    
             
-    return net, loss, running_loss
+    return net, top1.avg
             
 def valid_batch(epoch, dataloader, net, criterion, optimizer, log_freq=2000):
-    net.eval()
-    running_loss = 0.0
-    for i, data in tqdm(enumerate(dataloader, 0)):
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
-        
-        with torch.no_grad():
-            out = net(inputs)
-            loss = criterion(out,labels)
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(dataloader),
+        [batch_time, losses, top1, top5],
+        prefix='Test: ')
 
-        # print statistics
-        running_loss += loss.cpu().item()
-        if i % log_freq == log_freq-1:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / log_freq))
-            running_loss = 0.0
+    net.eval()
+    
+    running_loss = 0.0
+    with torch.no_grad():
+        end = time.time()
+        for i, data in tqdm(enumerate(dataloader, 0)):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
             
-    return net, loss, running_loss
+            output = net(inputs)
+            loss = criterion(output,labels)
+            
+            acc1, acc5 = accuracy(output,labels, topk=(1,5))
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(acc1[0], inputs.size(0))
+            top5.update(acc5[0], inputs.size(0))
+
+            batch_time.update(time.time() - end)
+            end = time.time()
+            
+            if i % log_freq == 0:
+                progress.display(i)
+
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+            .format(top1=top1, top5=top5))
+
+    return net, top1.avg
 
 
 def train_network(epoch, tloader, vloader, net, criterion, optimizer, log_freq=2000):
     for ep in tqdm(range(epoch)):
         train_batch(ep, tloader, net, criterion, optimizer, log_freq=log_freq)
         valid_batch(ep, vloader, net, criterion, optimizer, log_freq=log_freq)
+
+
+
 
