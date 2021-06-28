5e554d7b8e78979c9e5bbb711364e58cf7617be9
Gaung Alif <gaungalif@gmail.com>
2021-06-28 08:23:37 +0800

    refactor: MobileNet

--- cifar/models/mobile.py
+++ cifar/models/mobile.py
@@ -1,41 +1,55 @@
 import torch
 import torch.nn as nn
+from torch.nn.modules.conv import Conv2d
 
 
 
-class Depthwise(nn.Module):
-    def __init__(self, inplanes, outplanes, stride, padding=1,
-                kernel_size = 3, bias=False):
-        super(Depthwise, self).__init__()
-        self.inplanes = inplanes
-        self.depthwise = nn.Conv2d(inplanes, inplanes, kernel_size=kernel_size, groups=inplanes, padding=padding, stride=stride, bias=bias)
-        self.pointwise = nn.Conv2d(inplanes, outplanes, kernel_size=kernel_size, bias=bias)
-        self.separable = torch.nn.Sequential(self.depthwise, self.pointwise)
-        self.conv1x1 = nn.Conv2d(outplanes, outplanes, kernel_size=1)
-        self.bn = nn.BatchNorm2d(outplanes)
-        self.relu = nn.ReLU(inplace=True)
-        
-    def forward(self,x):
-        x = self.separable(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        x = self.conv1x1(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        return x
+class depthwise(nn.Module):
+    def __init__(self, inplanes, kernel_size=3, stride=1, padding=1, bias=False):
+        super(depthwise, self).__init__()
+        self.depthwise = nn.Conv2d(inplanes, inplanes, kernel_size=kernel_size, stride=stride, padding=padding, groups=inplanes, bias=bias)
 
-class SingleConvLayer(nn.Module):
-    def __init__(self, inplanes, outplanes, kernel_size=1, stride=1):
-        super(SingleConvLayer, self).__init__()
-        self.conv = nn.Conv2d(in_channels=inplanes, out_channels=outplanes, kernel_size=kernel_size, stride=stride)
-        self.bn = nn.BatchNorm2d(outplanes)
-        self.relu = nn.ReLU(inplace=True)
+    def forward(self, x):
+        out = self.depthwise(x)
+        return out
+
+class DepthConv(nn.Module):
+    def __init__(self, inplanes, kernel_size=3, stride=1,padding=1, bias=False):
+        super(DepthConv, self).__init__()
+        self.dw_block = nn.Sequential(
+            depthwise(inplanes, kernel_size, stride, padding, bias),
+            nn.BatchNorm2d(inplanes),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        out = self.dw_block(x)
+        return out
+
+class OneConv(nn.Module):
+    def __init__(self, inplanes, outplanes, padding=0, kernel_size=1, stride=1, bias=False):
+        super(OneConv, self).__init__()
+        self.one_conv = nn.Sequential(
+            nn.Conv2d(inplanes, outplanes, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias ),
+            nn.BatchNorm2d(outplanes),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        out = self.one_conv(x)
+        return out
+
+
+class ThreeConv(nn.Module):
+    def __init__(self, inplanes, outplanes, kernel_size=3, stride=2, padding=1, bias=False):
+        super(ThreeConv, self).__init__()
+        self.three_conv = nn.Sequential(
+            nn.Conv2d(inplanes, outplanes, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+            nn.BatchNorm2d(outplanes),
+            nn.ReLU(inplace=True)
+        )
         
     def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        return x
+        out = self.three_conv(x)
+        return out
 
 class Flatten(nn.Module):
     def forward(self, input):
@@ -47,35 +61,48 @@ class MobileNet(nn.Module):
         self.num_classes = num_classes
         
         self.features = nn.Sequential(
-            SingleConvLayer(3, 32, kernel_size=3, stride=2),
-            Depthwise(32, 32,stride=1, padding=2),
-            SingleConvLayer(32, 64, kernel_size=1, stride=1),
-            Depthwise(64, 64,stride=2, padding=3 ),         
-            SingleConvLayer(64, 128, kernel_size=1, stride=1),
-            Depthwise(128, 128, stride=1, padding=2),
-            SingleConvLayer(128, 128, kernel_size=1, stride=1),
-            Depthwise(128, 128, stride=2, padding=3),
-            SingleConvLayer(128, 256, kernel_size=1, stride=1),
-            Depthwise(256, 256, stride=1, padding=2),
-            SingleConvLayer(256, 256, kernel_size=1, stride=1),           
-            Depthwise(256, 256, stride=2, padding=3),
-            SingleConvLayer(256, 512, kernel_size=1, stride=1),
-            #5 times          
-            Depthwise(512, 512, stride=1, padding=2),
-            SingleConvLayer(512, 512, kernel_size=1, stride=1),
-            Depthwise(512, 512, stride=1, padding=2),
-            SingleConvLayer(512, 512, kernel_size=1, stride=1),
-            Depthwise(512, 512, stride=1, padding=2),
-            SingleConvLayer(512, 512, kernel_size=1, stride=1),
-            Depthwise(512, 512, stride=1, padding=2),
-            SingleConvLayer(512, 512, kernel_size=1, stride=1),
-            Depthwise(512, 512, stride=1, padding=2),
-            SingleConvLayer(512, 512, kernel_size=1, stride=1),
-            #end 5 times
-            Depthwise(512, 512, stride=2, padding=3),
-            SingleConvLayer(512, 1024, kernel_size=1, stride=1),
-            Depthwise(1024, 1024, stride=2, padding=6),
-            SingleConvLayer(1024, 1024, kernel_size=1, stride=1),
+            ThreeConv(3, 32),
+            DepthConv(32, stride=1),
+            OneConv(32, 32),
+            OneConv(32, 64),
+            DepthConv(64, stride=2),  
+            OneConv(64, 64),
+            OneConv(64, 128),       
+            DepthConv(128, stride=1),
+            OneConv(128, 128),
+            OneConv(128, 128), 
+            DepthConv(128, stride=2),
+            OneConv(128, 128),
+            OneConv(128, 256),
+            DepthConv(256, stride=1),
+            OneConv(256, 256),
+            OneConv(256, 256),
+            DepthConv(256, stride=2),
+            OneConv(256, 256),
+            OneConv(256, 512),
+            #5
+            DepthConv(512, stride=1),
+            OneConv(512, 512),
+            OneConv(512, 512),
+            DepthConv(512, stride=1),
+            OneConv(512, 512),
+            OneConv(512, 512),
+            DepthConv(512, stride=1),
+            OneConv(512, 512),
+            OneConv(512, 512),
+            DepthConv(512, stride=1),
+            OneConv(512, 512),
+            OneConv(512, 512),
+            DepthConv(512, stride=1),
+            OneConv(512, 512),
+            OneConv(512, 512),
+            #5 end
+            DepthConv(512, stride=2),
+            OneConv(512, 512),
+            OneConv(512, 1024),
+            DepthConv(1024, stride=2, padding=4),
+            OneConv(1024, 1024),
+            OneConv(1024, 1024),
             nn.AvgPool2d(7,7),
         )
 
