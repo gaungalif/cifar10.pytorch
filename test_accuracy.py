f9a72467ea92be75dac42a1eaa24a45e1627def1
Gaung Alif <gaungalif@gmail.com>
2021-07-07 02:39:56 +0800

    refactor: Progress and Average Function

--- test_accuracy.py
+++ test_accuracy.py
@@ -53,7 +53,8 @@ imgs, lbls = next(iter(valid_loader))
 imgs, lbls = imgs.to(device), lbls.to(device)
 out = NET(imgs)
 out = torch.softmax(out, dim=0)
-out = torch.argmax(out, dim=1)
+if NET != 'mbb':
+    out = torch.argmax(out, dim=1)
 # torch.argmax(out[0])
 true = 0
 false = 0
