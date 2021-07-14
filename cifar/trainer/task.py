3c397c15ac65b4621e18c56192cfc40267e15f0f
Gaung Alif <gaungalif@gmail.com>
2021-07-15 07:53:48 +0800

    feat: add learning rate decay

--- cifar/trainer/task.py
+++ cifar/trainer/task.py
@@ -103,10 +103,12 @@ def valid_batch(dataloader, net, criterion, log_freq=2000):
 
 def train_network(epoch, tloader, vloader, net, criterion, optimizer, log_freq=2000):
     global best_acc1
+    decayRate = 0.98
+    my_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=decayRate)
     for ep in tqdm(range(epoch)):
         train_batch(ep, tloader, net, criterion, optimizer, log_freq=log_freq)
         valid_batch(vloader, net, criterion,  log_freq=log_freq)
-        
+        my_lr_scheduler.step()
         acc1 = valid_batch(vloader, net, criterion, log_freq=log_freq)
         is_best = acc1 > best_acc1
         best_acc1 = max(acc1, best_acc1)
